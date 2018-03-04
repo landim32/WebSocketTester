6633661a7af1c34dc330d37716f9441d6c80fccc
Rodrigo <rodrigo@emagine.com.br>
2018-03-04 12:04:52 -0300

    Pull inicial

---
+++ WebSocketTester/PrincipalForm.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace WebSocketTester
+{
+    partial class PrincipalForm
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.UrlTextBox = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.envioTextBox = new System.Windows.Forms.TextBox();
+            this.logTextBox = new System.Windows.Forms.TextBox();
+            this.GoBtn = new System.Windows.Forms.Button();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.GoBtn);
+            this.panel1.Controls.Add(this.UrlTextBox);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(380, 27);
+            this.panel1.TabIndex = 2;
+            // 
+            // UrlTextBox
+            // 
+            this.UrlTextBox.AcceptsReturn = true;
+            this.UrlTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.UrlTextBox.Location = new System.Drawing.Point(3, 3);
+            this.UrlTextBox.Name = "UrlTextBox";
+            this.UrlTextBox.Size = new System.Drawing.Size(265, 20);
+            this.UrlTextBox.TabIndex = 1;
+            this.UrlTextBox.Text = "ws://localhost:8080/";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.splitContainer1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 27);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(380, 334);
+            this.panel2.TabIndex = 3;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.SendButton);
+            this.splitContainer1.Panel1.Controls.Add(this.envioTextBox);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.logTextBox);
+            this.splitContainer1.Size = new System.Drawing.Size(380, 334);
+            this.splitContainer1.SplitterDistance = 105;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // envioTextBox
+            // 
+            this.envioTextBox.AcceptsReturn = true;
+            this.envioTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.envioTextBox.Location = new System.Drawing.Point(0, 0);
+            this.envioTextBox.Multiline = true;
+            this.envioTextBox.Name = "envioTextBox";
+            this.envioTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.envioTextBox.Size = new System.Drawing.Size(377, 79);
+            this.envioTextBox.TabIndex = 3;
+            // 
+            // logTextBox
+            // 
+            this.logTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.logTextBox.Location = new System.Drawing.Point(0, 0);
+            this.logTextBox.Multiline = true;
+            this.logTextBox.Name = "logTextBox";
+            this.logTextBox.ReadOnly = true;
+            this.logTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.logTextBox.Size = new System.Drawing.Size(380, 225);
+            this.logTextBox.TabIndex = 4;
+            // 
+            // GoBtn
+            // 
+            this.GoBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.GoBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GoBtn.Location = new System.Drawing.Point(274, 2);
+            this.GoBtn.Name = "GoBtn";
+            this.GoBtn.Size = new System.Drawing.Size(103, 22);
+            this.GoBtn.TabIndex = 2;
+            this.GoBtn.Text = "CONNECT";
+            this.GoBtn.UseVisualStyleBackColor = true;
+            this.GoBtn.Click += new System.EventHandler(this.GoBtn_Click);
+            // 
+            // SendButton
+            // 
+            this.SendButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.SendButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SendButton.Location = new System.Drawing.Point(274, 80);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(103, 22);
+            this.SendButton.TabIndex = 4;
+            this.SendButton.Text = "SEND";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // PrincipalForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 361);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "PrincipalForm";
+            this.Text = "WebSocket Tester 0.1a";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox UrlTextBox;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox envioTextBox;
+        private System.Windows.Forms.TextBox logTextBox;
+        private System.Windows.Forms.Button GoBtn;
+        private System.Windows.Forms.Button SendButton;
+    }
+}
+
---
+++ WebSocketTester/PrincipalForm.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WebSocketSharp;
+
+namespace WebSocketTester
+{
+    public partial class PrincipalForm : Form
+    {
+        private WebSocket ws = null;
+
+        public PrincipalForm()
+        {
+            InitializeComponent();
+            //disableSendBtn();
+        }
+
+        private void createWebSocket() {
+            ws = new WebSocket(UrlTextBox.Text);
+            ws.WaitTime = TimeSpan.FromSeconds(10);
+            ws.OnOpen += Ws_OnOpen;
+            ws.OnClose += Ws_OnClose;
+            ws.OnError += Ws_OnError;
+            ws.OnMessage += Ws_OnMessage;
+            ws.ConnectAsync();
+            logTextBox.AppendText("Try connect to " + UrlTextBox.Text + "...\r\n");
+        }
+
+        private void enableConnectBtn() {
+            if (InvokeRequired)
+            {
+                Invoke((MethodInvoker)delegate
+                {
+                    GoBtn.Text = "DISCONNECT";
+                    logTextBox.AppendText("Connect!\r\n");
+                });
+            }
+            else {
+                GoBtn.Text = "DISCONNECT";
+                logTextBox.AppendText("Connect!\r\n");
+            }
+        }
+
+        private void disableConnectBtn() {
+            if (InvokeRequired)
+            {
+                Invoke((MethodInvoker)delegate
+                {
+                    GoBtn.Text = "CONNECT";
+                    logTextBox.AppendText("Disconnected!\r\n");
+                });
+            }
+            else
+            {
+                GoBtn.Text = "CONNECT";
+                logTextBox.AppendText("Disconnected!\r\n");
+            }
+        }
+
+        private void enableSendBtn() {
+            SendButton.Text = "SEND";
+            SendButton.Enabled = true;
+        }
+
+        private void disableSendBtn() {
+            SendButton.Text = "SENDING...";
+            SendButton.Enabled = false;
+        }
+
+        private void GoBtn_Click(object sender, EventArgs e)
+        {
+            if (ws != null)
+            {
+                if (ws.IsAlive)
+                {
+                    ws.Close();
+                    ws = null;
+                    //disableConnectBtn();
+                }
+                else
+                {
+                    createWebSocket();
+                }
+            }
+            else {
+                createWebSocket();
+            }
+        }
+
+        private void Ws_OnOpen(object sender, EventArgs e)
+        {
+            enableConnectBtn();
+
+        }
+
+        private void Ws_OnClose(object sender, CloseEventArgs e)
+        {
+            disableConnectBtn();
+        }
+
+        private void Ws_OnMessage(object sender, MessageEventArgs e)
+        {
+            if (InvokeRequired)
+            {
+                Invoke((MethodInvoker)delegate {
+                    logTextBox.AppendText("--received----------------\r\n");
+                    logTextBox.AppendText(e.Data + "\r\n");
+                });
+            }
+           
+        }
+
+        private void Ws_OnError(object sender, ErrorEventArgs e)
+        {
+            if (InvokeRequired)
+            {
+                Invoke((MethodInvoker)delegate {
+                    logTextBox.AppendText("Error: " + e.Message + "\r\n");
+                    SendButton.Text = "Send";
+                    SendButton.Enabled = true;
+                });
+            }
+          
+        }
+
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+            if (ws != null && ws.IsAlive)
+            {
+                disableSendBtn();
+                var texto = envioTextBox.Text;
+                envioTextBox.Text = "";
+                ws.SendAsync(texto, (bool enviado) =>
+                {
+                    if (InvokeRequired)
+                    {
+                        Invoke((MethodInvoker)delegate
+                        {
+                            logTextBox.AppendText("--sended----------------\r\n");
+                            logTextBox.AppendText(texto + "\r\n");
+                            enableSendBtn();
+                        });
+                    }
+                });
+            }
+            else {
+                logTextBox.AppendText("Not connected!\r\n");
+                //enableSendBtn();
+            }
+        }
+    }
+}
